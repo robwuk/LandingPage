02398b3ab3a63ef76b9e6cd0d933b075bc9e623c
Robert Worth <44876521+robwuk@users.noreply.github.com>
2018-11-19 17:49:40 +0000

    Delete MPC_Script.js

--- MPC_Script.js
+++
@@ -1,8 +0,0 @@
-function alertAge(age, thresh) {
-    var isAdult = (age < thresh) ? "too young": "old enough";
-
-    alert("you are " + isAdult + " (" + age + "). Theshold is " + thresh +".");
-}
-
-alertAge(19,18);
-alertAge(16,20);
