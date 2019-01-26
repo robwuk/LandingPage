df9cb4dd3a5eddf583f6fb3fcdfaacd59dcc9b1f
Robert Worth <robertworth@hotmail.com>
2019-01-26 15:05:46 +0000

    split JS files completed HOME

--- data.js
+++ data.js
@@ -1,17 +1,28 @@
-var navigation =[
+const author = "Robert Worth";
+const intro = " ~ Personal Profile ~";
+
+var navigation = [
                   ["HOME", "HOME", "nav1"],
                   ["Biography", "ABOUT", "nav1"],
                   ["Personal Projects", "PERSONAL", "nav1"],
-                  ["freeCodeCamp Responsive Web Desgn", "FCCRWD", "nav1"],
-                  ["freeCodeCamp Front End Libraries", "FCCFEL", "nav1"],
+                  //["freeCodeCamp Responsive Web Desgn", "FCCRWD", "nav1"],
+                  //["freeCodeCamp Front End Libraries", "FCCFEL", "nav1"],
+                  ["Online Coursework","COURSES", "nav1"],
                   ["Certificates", "CERTS", "nav1"]
                 ];
 
-var personalNav =[
-                  ["Monkton Park Church", "MPC", "https://robwuk.github.io/MPC_Boot/", "MPC_Old_Photo.jpg", ["JavaScript", "HTML", "Bootstrap", "CSS"], "Development website for Monkton Park Methodist Church", "nav2"],
-                  ["Sandbox", "SANDBOX", "https://robwuk.github.io/MPC_Boot/", "WIP.jpg", ["JavaScript", "HTML", "Bootstrap", "CSS"], "Development website I use for trying new techniques", "nav2"]];
+var courseNav = [
+                  ["freeCodeCamp Responsive Web Desgn", "FCCRWD", "", "", "", "", "nav2"],
+                  ["freeCodeCamp Front End Libraries", "FCCFEL", "", "", "", "", "nav2"],
+                  ["Frontend Mentor", "FEM", "", "", "","", "nav2"]
+                ];
 
-var fccRWDNav =[
+var personalNav = [
+                  ["Monkton Park Church", "MPC", "https://robwuk.github.io/MPC_Boot/", "MPC_Old_Photo.jpg", ["JavaScript", "HTML", "Bootstrap", "CSS"], "Development website for Monkton Park Methodist Church", "nav2"],
+                  ["Sandbox", "SANDBOX", "https://robwuk.github.io/MPC_Boot/", "WIP.jpg", ["JavaScript", "HTML", "Bootstrap", "CSS"], "Development website I use for trying new techniques", "nav2"]
+                ];
+
+var fccRWDNav = [
                   ["Tribute Page", "TRIBUTE", "https://robwuk.github.io/TributePage/", "Tribute.PNG", ["HTML", "CSS"], "freeCodeCamp Tribute Page", "nav2"],
                   ["Technical Documentation", "TECHDOC", "https://robwuk.github.io/TechDoc/", "TechDoc.PNG", ["HTML", "CSS"], "freeCodeCamp Technical Documentation Page", "nav2"],
                   ["Survey Form", "SURVEY", "https://robwuk.github.io/SurveyForm/", "Survey.PNG", ["HTML", "CSS"], "freeCodeCamp Survey Form Page", "nav2"],
@@ -19,7 +30,7 @@ var fccRWDNav =[
                   ["Product Overview", "PRODUCT", "https://robwuk.github.io/ProductPage/", "Product.PNG", ["HTML", "CSS"], "freeCodeCamp Product Overview Page", "nav2"]
                 ];
 
-var fccFELNav =[
+var fccFELNav = [
                   ["Random Quote Generator", "RQG", "https://robwuk.github.io/FCC_RQG/", "Tribute.PNG", ["HTML", "CSS", "JavaScript", "Bootstrap"], "freeCodeCamp Random Quote Generator", "nav2"],
                   ["Calculator", "CALC", "https://robwuk.github.io/FCC_Calc/", "TechDoc.PNG", ["HTML", "CSS", "JavaScript", "Bootstrap"], "freeCodeCamp Javascript Calculator", "nav2"],
                   ["Drumkit", "DRUMKIT", "https://robwuk.github.io/fcc_drum/", "Survey.PNG", ["HTML", "CSS", "JavaScript", "Bootstrap"], "freeCodeCamp Javascriupt Drumkit", "nav2"],
@@ -27,62 +38,24 @@ var fccFELNav =[
                   ["GitHub Markdown Previewer", "MARKDOWN", "https://robwuk.github.io/Git_Parse/", "Product.PNG", ["HTML", "CSS"], "freeCodeCamp GitHub Markdown Previewer", "nav2"]
                 ];
 
+var home = [
+            ["Robert Worth", "home__name", "author"],
+            ["Welcome to my Project Landing Page", "home__welcome", "welcome"]
+]
 
+var quotes = [
+              ["For the best return on your money, pour your purse into your head.", "Benjamin Franklin"],
+              ["Absorb what is useful, Discard what is not, Add what is uniquely your own.", "Bruce Lee"],
+              ["Between stimulus and response there is a space. In that space is our power to choose our response. In our response lies our growth and our freedom.", "Vikto Frankl"],
+              ["We must become the change we want to see.", "Mahatma Ghandi"],
+              ["Investing in yourself is the best investment you will ever make. It will not only improve your life, it will improve the lives of all those around you.", "Robin Sharma"],
+              ["Look in the mirror that’s your competition.", "Anon."],
+              ["The only person you are destined to become is the person you decide to be.", "Ralph Waldo Emerson"]
+]
 
-var navDivOne = document.getElementById("navigation");
-var navDivTwo = document.getElementById("subnav");
-
-function nav1OnClick(caller) {
-  switch (caller) {
-    case "HOME":
-      secondNavVisible("hidden");
-      break;
-    case "PERSONAL":
-      createNav2(personalNav);
-      break;
-    case "FCCRWD":
-      createNav2(fccRWDNav);
-      break;
-    case "FCCFEL":
-      createNav2(fccFELNav);
-      break;
-  };
-}
-
-function secondNavVisible(visible){
-  navDivTwo.style.visibility = visible;
-}
-
-function createNav2(navArray) {
-  secondNavVisible("hidden");
-  navDivTwo.innerHTML="";
-  for (i=0; i<navArray.length;i++) {
-    var node = document.createElement("span");
-    console.log("here");
-    node.innerHTML = navArray[i][0];
-    node.id = navArray[i][1];
-    node.className = navArray[i][6];
-    navDivTwo.appendChild(node);
-  }
-
-  secondNavVisible("visible");
-}
-
-function createPage(){
-
-  secondNavVisible("hidden");
-
-  for (i=0; i<navigation.length;i++) {
-    var node = document.createElement("span");
-    console.log(navigation[i][0]);
-    node.innerHTML = navigation[i][0];
-    node.id = navigation[i][1];
-    node.className = navigation[i][2];
-    node.setAttribute("onclick", "nav1OnClick('"+navigation[i][1]+"')");
-    navDivOne.appendChild(node);
-  }
-  //createNav2(personalNav);
-
-}
-
-window.onload = createPage();
+var techIcons = [
+                  ["fab fa-html5", "HTML5"],
+                  ["fab fa-css3-alt", "CSS3"],
+                  ["fab fa-js-square", "Javascript"],
+                  ["fab fa-angular", "Angular"]
+                ]
---
+++ navigation.js
@@ -0,0 +1,54 @@
+var mainArea = document.getElementById("mainarea");
+var navDivOne = document.getElementById("navigation");
+var navDivTwo = document.getElementById("subnav");
+
+function nav1OnClick(caller) {
+  switch (caller) {
+    case "HOME":
+      secondNavVisible("hidden");
+      homePage();
+      break;
+    case "PERSONAL":
+      createNav2(personalNav);
+      break;
+    case "COURSES":
+      createNav2(courseNav);
+      break;
+  //  case "FCCRWD":
+  //    createNav2(fccRWDNav);
+  //    break;
+  //  case "FCCFEL":
+  //    createNav2(fccFELNav);
+  //    break;
+  };
+}
+
+function secondNavVisible(visible){
+  navDivTwo.style.visibility = visible;
+}
+
+function createNav1() {
+  for (i=0; i<navigation.length;i++) {
+    var node = document.createElement("span");
+    console.log(navigation[i][0]);
+    node.innerHTML = navigation[i][0];
+    node.id = navigation[i][1];
+    node.className = navigation[i][2];
+    node.setAttribute("onclick", "nav1OnClick('"+navigation[i][1]+"')");
+    navDivOne.appendChild(node);
+  }
+}
+
+function createNav2(navArray) {
+  secondNavVisible("hidden");
+  navDivTwo.innerHTML="";
+  for (i=0; i<navArray.length;i++) {
+    var node = document.createElement("span");
+    node.innerHTML = navArray[i][0];
+    node.id = navArray[i][1];
+    node.className = navArray[i][6];
+    navDivTwo.appendChild(node);
+  }
+
+  secondNavVisible("visible");
+}
